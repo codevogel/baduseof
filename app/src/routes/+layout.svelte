efb9e68abc0d7d98697c1392427f95be5821b35d
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-04 21:32:07 +0100

    feat(routes): add site title tag

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,12 +2,16 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
+	import { getTitleByPathname } from '$lib/assets/routes';
+	import { page } from '$app/state';
 
 	let { children } = $props();
+	let pageTitle = $derived(getTitleByPathname(page.url.pathname));
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>{pageTitle}</title>
 </svelte:head>
 
 <ModeWatcher />
