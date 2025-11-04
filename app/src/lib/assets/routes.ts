4d454bfd655ca001a879291bce78a1ca937a410b
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-04 23:43:14 +0100

    feat: add overview, about, and topic pages

--- app/src/lib/assets/routes.ts
+++ app/src/lib/assets/routes.ts
@@ -9,13 +9,40 @@ export const routes: Route[] = [
 		path: '/',
 		title: 'Home',
 		name: 'home'
+	},
+	{
+		path: '/overview',
+		title: 'Overview',
+		name: 'overview'
+	},
+	{
+		path: '/about',
+		title: 'About',
+		name: 'about'
+	}
+];
+
+export type Topic = {
+	slug: string;
+	title: string;
+};
+
+export const topics: Topic[] = [
+	{
+		slug: 'ai',
+		title: 'AI'
 	}
 ];
 
 const baseTitle = 'Bad Use Of';
 
-export function getTitleByPathname(pathname: string): string | undefined {
-	if (pathname === '/') return baseTitle;
-	const route = routes.find((r) => r.path === pathname);
-	return route ? `${baseTitle} ${route.title}` : baseTitle;
+export function getTitleByPathname(pathname: string): string {
+	const paths = routes.map((r) => r.path);
+	if (paths.includes(pathname)) return baseTitle;
+
+	const slug = pathname.split('/')[1];
+	const topic = topics.find((t) => t.slug === slug);
+
+	if (!topic) return baseTitle;
+	return `${baseTitle}: ${topic.title}`;
 }
