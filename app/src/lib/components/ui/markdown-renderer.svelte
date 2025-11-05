e1cff7b8cd9211c39f6150db571b543902d497be
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-05 16:33:33 +0100

    feat: add markdown-renderer component

---
+++ app/src/lib/components/ui/markdown-renderer.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	// --- exmarkdown imports ---
+	import type { Plugin } from 'svelte-exmarkdown';
+	import Markdown from 'svelte-exmarkdown';
+	import { gfmPlugin } from 'svelte-exmarkdown/gfm';
+
+	// --- rehype imports ---
+	// plugins
+	import rehypeSlug from 'rehype-slug';
+
+	import { ShikiHighlighter } from '$lib/utils/shiki';
+	import rehypeShikiFromHighlighter from '@shikijs/rehype/core';
+	import Article from '$lib/components/ui/article.svelte';
+
+	let { mdContent }: { mdContent: string } = $props();
+
+	const shikiPlugin = {
+		rehypePlugin: [
+			rehypeShikiFromHighlighter,
+			ShikiHighlighter.getInstance(),
+			{ theme: 'kanagawa-wave' }
+		]
+	} satisfies Plugin;
+
+	const slugPlugin = {
+		rehypePlugin: [rehypeSlug]
+	} satisfies Plugin;
+
+	const plugins: Plugin[] = [gfmPlugin(), shikiPlugin, slugPlugin];
+</script>
+
+<Article>
+	<Markdown md={mdContent} {plugins} />
+</Article>
