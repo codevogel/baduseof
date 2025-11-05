e6471208ad41ea185a4e8653158312941830ca2e
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-05 16:33:27 +0100

    feat: add article component

---
+++ app/src/lib/components/ui/article.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<article class="mx-auto my-8 prose max-w-lg px-8 dark:prose-invert prose-blockquote:border-primary">
+	{@render children()}
+</article>
