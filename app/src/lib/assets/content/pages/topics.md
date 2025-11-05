760d97eaecbaca5071aea6140826fdc7ddebde28
Kamiel de Visser <kamieldevisser@gmail.com>
2025-11-06 00:24:32 +0100

    feat: stream in topics to topics.md

--- app/src/lib/assets/content/pages/topics.md
+++ app/src/lib/assets/content/pages/topics.md
@@ -1,5 +1,8 @@
 # Topics
 
-Here is an overview of the topics covered by **Bad Use Of**
+This page serves as a gallery of questionable choices. Here you'll find the
+concepts, tools, and practices that **Bad Use Of** shines a light on.
 
-- [**AI**](/ai)
+> Frustrated by bad use of something not listed here? We'd love to hear about
+> it! Feel free to suggest new topics or contribute to existing ones through our
+> [GitHub repository](https://github.com/codevogel/baduseof).
--- app/src/routes/topics/+page.ts
+++ app/src/routes/topics/+page.ts
@@ -1,9 +1,19 @@
 import type { PageLoad } from './$types';
 
-import topics from '$lib/assets/content/pages/topics.md?raw';
+import topicMd from '$lib/assets/content/pages/topics.md?raw';
+import { topics, type Topic } from '$lib/assets/topics';
 
 export const load: PageLoad = () => {
+	const mdContent = appendTopicList(topicMd, topics);
 	return {
-		mdContent: topics
+		mdContent
 	};
 };
+
+function appendTopicList(md: string, topics: Topic[]) {
+	const sortedTopics = topics.slice().sort((a, b) => a.title.localeCompare(b.title));
+	sortedTopics.forEach((topic) => {
+		md += `- [${topic.title}](/topics/${topic.slug})\n`;
+	});
+	return md;
+}
